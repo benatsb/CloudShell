37268adce913eb0bba6f70a47424d64625dc753a
Darren Tu <darrentu@users.noreply.github.com>
2023-01-30 10:59:32 -0800

    Update generateVersion.ps1

--- .pipelines/generateVersion.ps1
+++ .pipelines/generateVersion.ps1
@@ -4,7 +4,7 @@ $minor = "0"
 $date_patch = Get-Date -UFormat "%Y%m%d"
 $BUILD_COUNT_DAY = $env:CDP_DEFINITION_BUILD_COUNT_DAY
 $commit = $env:CDP_COMMIT_ID
-$branch = $env:BUILD_SOURCEBRANCHNAME.ToLower()
+$branch = $env:GithubBranch.ToLower()
 $build_tag = $env:CDP_BUILD_TAG.ToLower()
 $buildId = "$build_tag.$branch.$commit.$date_patch.$BUILD_COUNT_DAY"
 [Environment]::SetEnvironmentVariable("CustomBuildNumber", $buildId)  # This will allow you to use it from env var in later steps of the same phase
